c83145e030bda5ceae52303117d04e37ea527c3e
DezareD <dezare3232@gmail.com>
2022-06-10 10:06:31 +0300

    Init

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Geocoder from 'react-native-geocoding';
 
-export default function App() {
+import Toast from 'react-native-toast-message';
+
+import UserContextProvider from './src/contexts/UserContext';
+import AnnouncementContextProvider from './src/contexts/AnnoucementContext';
+import Router from './src/routes/Routes';
+
+import { toastConfig } from './src/style/toastStyles';
+
+import { StatusBar } from 'react-native';
+import { setCustomText } from 'react-native-global-props';
+import { typography } from './src/style/typography';
+import { colors } from './src/style/colors';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './src/components/TabNavigation';
+
+
+const App = () => {
+  const apiKey = "boba"
+  Geocoder.init(apiKey);
+
+  const customTextProps = {
+    style: {
+      fontFamily: typography.raleway.regular,
+      color: colors.black.main,
+    }
+  }
+  setCustomText(customTextProps);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <UserContextProvider>
+      <AnnouncementContextProvider>
+        <StatusBar translucent backgroundColor="transparent" barStyle='dark-content' />
+        <Router />
+        <Toast config={toastConfig} />
+      </AnnouncementContextProvider>
+    </UserContextProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
---
+++ src/style/typography.js
@@ -0,0 +1,14 @@
+export const typography = {
+  geo: 'Roboto',
+  raleway: {
+    regular: 'Roboto',
+    medium: 'Roboto',
+    bold: 'Roboto',
+  },
+  inter: {
+    light: 'Roboto',
+    bold: 'Roboto',
+    semiBold: 'Roboto',
+    regular: 'Roboto',
+  },
+}
